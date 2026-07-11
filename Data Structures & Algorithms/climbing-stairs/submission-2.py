c5a5eb17a4ecb6e0212d35fdf44582e36f306a1d
ShadabShahzada <126250733+ShahzadaShadabAli@users.noreply.github.com>
2026-07-11 11:46:41 +0500

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # memo = {}
+        # def check(n):
+        #     if memo.get(n, False): return memo[n]
+        #     if n == 1:
+        #         return 1
+        #     if n == 2:
+        #         return 2
+        #     res = check(n-1)+check(n-2)
+        #     memo[n] = res
+        #     return res
+        # return check(n)
+        one, two = 1, 1
+
+        for i in range(n-1):
+            temp = one
+            one+=two
+            two=temp
+        return one
